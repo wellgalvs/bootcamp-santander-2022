4829d2ee077fa203530ec8e988ce4ccc5229a918
Wellington G. Alves <wellingtonalvs@gmail.com>
2022-06-21 15:35:47 -0300

    Habilitar/Desabilitar Incremento/Decremento

--- html-css3-js/4-intro-javascript/5-colocando-em-pratica/assets/js/scripts.js
+++ html-css3-js/4-intro-javascript/5-colocando-em-pratica/assets/js/scripts.js
@@ -7,14 +7,34 @@ const CURRENT_NUMBER = document.getElementById('currentNumber');
 
 function increment() {
   count++;
+  if (CURRENT_NUMBER > 10) {
+    btnIncrement.disabled = true;
+  }
+
+  if (count == 11) {
+    count--;
+  }
+
+  btnIncrement.disabled = false;
+
   CURRENT_NUMBER.innerHTML = count;
 }
 
 function decrement() {
   count--;
+  if (CURRENT_NUMBER < 0) {
+    btnDecrement.disabled = true;
+  }
+
+  if (count == -1) {
+    count++;
+  }
+
+  btnDecrement.disabled = false;
+
   CURRENT_NUMBER.innerHTML = count;
 }
 
 btnIncrement.addEventListener('click', increment);
 
-btnDecrement.addEventListener('click', decrement); 
+btnDecrement.addEventListener('click', decrement);
