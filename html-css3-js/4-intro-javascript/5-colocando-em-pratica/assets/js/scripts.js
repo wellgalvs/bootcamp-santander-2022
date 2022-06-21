ac14a9073a995643c7f5d2632642339b71cd1ea7
Wellington G. Alves <wellingtonalvs@gmail.com>
2022-06-21 13:07:46 -0300

    Implementando o addEventListener

--- html-css3-js/4-intro-javascript/5-colocando-em-pratica/assets/js/scripts.js
+++ html-css3-js/4-intro-javascript/5-colocando-em-pratica/assets/js/scripts.js
@@ -1,5 +1,8 @@
 let count = 0;
 
+let btnIncrement = document.querySelector('.btnIncrement');
+let btnDecrement = document.querySelector('.btnDecrement');
+
 const CURRENT_NUMBER = document.getElementById('currentNumber');
 
 function increment() {
@@ -11,3 +14,7 @@ function decrement() {
   count--;
   CURRENT_NUMBER.innerHTML = count;
 }
+
+btnIncrement.addEventListener('click', increment);
+
+btnDecrement.addEventListener('click', decrement); 
