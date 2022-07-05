0af2b70e11b9b53402d822ea3866952d265ccf17
Wellington G. Alves <wellingtonalvs@gmail.com>
2022-07-05 16:54:50 -0300

    Desenvolvendo Condicionais a Partir de Parâmetros

---
+++ html-css3-js/15-intro-typescript-classes-tipos-interfaces/7-desenvolvendo-condicionais-de-parametros/index.ts
@@ -0,0 +1,16 @@
+interface IUsuario {
+  id: string;
+  email: string;
+}
+
+interface IAdmin extends IUsuario {
+  cargo: 'gerente' | 'coordenador' | 'supervisor';
+}
+
+function redirecione(usuario: IAdmin | IUsuario) {
+  if ('cargo' in usuario) {
+    // redirecionar para a área de administração
+  }
+
+  // redirecionar para a área de usuário
+}
