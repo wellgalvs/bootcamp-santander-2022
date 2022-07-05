fcbf9b19ac219a40f73e2cba368ab722a597a2d5
Wellington G. Alves <wellingtonalvs@gmail.com>
2022-07-05 14:35:55 -0300

    Tratando a Tag Input

---
+++ html-css3-js/15-intro-typescript-classes-tipos-interfaces/5-tratando-tag-input/index.ts
@@ -0,0 +1,7 @@
+const input = document.getElementById('input') as HTMLInputElement;
+
+input.addEventListener('input', (event) => {
+  // console.log('Digitei!');
+  const i = event.currentTarget as HTMLInputElement;
+  console.log(i.value);
+});
