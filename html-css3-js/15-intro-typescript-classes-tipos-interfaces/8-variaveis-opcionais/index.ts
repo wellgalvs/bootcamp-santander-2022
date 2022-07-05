16f3eab74ac797770f5f694290b87dc7b74b4146
Wellington G. Alves <wellingtonalvs@gmail.com>
2022-07-05 17:23:33 -0300

    Utilizando o Caracter ? para Indicar Variáveis Opcionais

---
+++ html-css3-js/15-intro-typescript-classes-tipos-interfaces/8-variaveis-opcionais/index.ts
@@ -0,0 +1,13 @@
+interface IUsuario {
+  id: string;
+  email: string;
+  cargo?: 'gerente' | 'coordenador' | 'supervisor' | 'funcionario';
+}
+
+function redirecione(usuario: IUsuario) {
+  if (usuario.cargo) {
+    // redirecionar(usuario.cargo);
+  }
+
+  // redirecionar para a área de usuário
+}
