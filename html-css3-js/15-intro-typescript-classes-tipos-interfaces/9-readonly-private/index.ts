0527ead689af59a1d5d99c320ce25f93f8f991ec
Wellington G. Alves <wellingtonalvs@gmail.com>
2022-07-06 09:46:40 -0300

    Readonly e Private

---
+++ html-css3-js/15-intro-typescript-classes-tipos-interfaces/9-readonly-private/index.ts
@@ -0,0 +1,21 @@
+interface ICachorro {
+  name: string;
+  idade: number;
+  parqueFavorito?: string;
+}
+
+type CachorroSomenteLeitura = {
+  readonly [K in keyof ICachorro]: ICachorro[K];
+}
+
+class MeuCachorro implements CachorroSomenteLeitura {
+  nome;
+  idade;
+
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+}
+
+const cao = new MeuCachorro('Dog', 13);
