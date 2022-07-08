e9a8210998d56b7ae9b33c56cf2f8d42c525171d
Wellington G. Alves <wellingtonalvs@gmail.com>
2022-07-08 13:45:39 -0300

    noImplicityAny e strictNullChecks

--- html-css3-js/16-introducao-pratica-ao-typescript/1-ts-preparacao-inicial-vscode/src/app.ts
+++ html-css3-js/16-introducao-pratica-ao-typescript/1-ts-preparacao-inicial-vscode/src/app.ts
@@ -1,5 +1,9 @@
 let buttonTeste = document.getElementById('button');
 
+/*function somarImplicito(params1, params2) {
+  return params1 + params2;
+}*/
+
 // "?" -> verificador
 buttonTeste?.addEventListener('click', () => {
   console.log('Clicou!');
